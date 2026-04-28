b69b6bf81d0643a44917d078a385bf9f3d35ef72
michaelbag8 <michaelbag8@gmail.com>
2026-04-28 11:00:53 +0100

    add: func

--- practice.go
+++ practice.go
@@ -29,6 +29,56 @@ func safeGetRune(s string, i int) rune {
 	}
 	return runes[i]
 }
+
+
+func prevWords(sentence string, i, n int) []string {
+	words := strings.Fields(sentence)
+	start := i - n
+
+	if start < 0 {
+		start = 0
+	}
+	if i <= 0 || i >= len(words) {
+		return []string{}
+	}
+	word := words[start:i]
+	return word
+
+}
+
+// returns the n words after position i
+func nextWords(sentence string, i, n int) []string {
+	words := strings.Fields(sentence)
+
+	if i < 0 || i >= len(words) {
+		return []string{}
+	}
+
+	start := i
+	end := i + 1 + n
+	if end > len(words) {
+		end = len(words)
+	}
+
+	return words[start:end]
+}
+
+func nWordsBefore(sentence string, i, n int) string {
+	words := strings.Fields(sentence)
+	if i <= 0 || i > len(words) {
+		return " "
+	}
+
+	start := i - n
+	if start < 0 {
+		start = 0
+	}
+
+	return strings.Join(words[start:i], " ")
+
+}
+
+
 func main() {
 
 	//firstAndLast words
@@ -50,5 +100,14 @@ func main() {
 	fmt.Println(safeGetRune(g, 0))
 	r := "hello"
 	fmt.Println(safeGetRune(r, 2))
+
+	sentence := "we offer them bread but they were counting the gold were we trade"
+	fmt.Println(prevWords(sentence, 5, 2))
+
+	z := "the quick brown fox"
+	fmt.Println(nextWords(z, 0, 1))
+
+	fmt.Println(nWordsBefore("one two three", 1, 5))
+
 
 }
